dd9db6c3a048649e883cff3fd120889e02b57d95
satria01muslim <126138744+satria01muslim@users.noreply.github.com>
2023-04-15 14:33:55 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata Saya</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f2f2f2;
+		}
+		h1 {
+			text-align: center;
+		}
+		table {
+			margin: auto;
+			border-collapse: collapse;
+			border: 1px solid #ddd;
+		}
+		th, td {
+			padding: 10px;
+			border: 1px solid #ddd;
+		}
+		img {
+			display: block;
+			margin: auto;
+			width: 150px;
+			height: 200px;
+			object-fit: cover;
+			border-radius: 50%;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+		}
+	</style>
+</head>
+<body>
+	<h1>Biodata Saya</h1>
+	<table>
+		<tr>
+			<th>Nama</th>
+			<td>Satriawan Muslim</td>
+		</tr>
+		<tr>
+			<th>TTL</th>
+			<td>Puyung, 16 september 1999</td>
+		</tr>
+		<tr>
+			<th>Jenis Kelamin</th>
+			<td>Laki-Laki</td>
+		</tr>
+		<tr>
+			<th>Alamat</th>
+			<td>Lombok, Nusa Tenggara Barat</td>
+		</tr>
+		<tr>
+			<th>Foto</th>
+			<td><img src="profil.jpg" alt="Satriawan Muslim"></td>
+		</tr>
+		<tr>
+			<th>Riwayat Pendidikan</th>
+			<td>
+				<ul>
+					<li>Sekolah Dasar Negeri 1 Puyung</li>
+					<li>Madrasah Tsanawiyah Negeri Jonggat</li>
+					<li>Ponpes Nurul Haramain</li>
+					<li>Universitas Bumigora</li>
+				</ul>
+			</td>
+		</tr>
+		<tr>
+			<th>Hobby</th>
+			<td>Travelling</td>
+		</tr>
+	</table>
+</body>
+</html>
